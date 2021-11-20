2092899a5aa34dabd0d409ffddd173bbc4eb0ce4
Chiara Caruso <carusochiara08@gmail.com>
2021-11-20 16:14:06 +0100

    Bene ma non benissimo!

--- js/app.js
+++ js/app.js
@@ -1,23 +1,36 @@
 const chicken = document.querySelector(".chick");
 const btns = [...document.querySelectorAll(".btn")];
 
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+
+        const id = btn.id;
+
+        let a = 0;
+        let b = 0;
+
+        switch (id) {
+            case 'up':
+                chicken.style.top = "150px"; 
+                chicken.style.backgroundPosition = "right -130px";
+                break;
+            case 'dx':
+                chicken.style.right = "150px";
+                chicken.style.backgroundPosition = "right -390px";
+                break;
+            case 'down':
+                chicken.style.bottom = "-150px"; 
+                chicken.style.backgroundPosition = "right 0px";
+                break;
+            case 'sx':
+                chicken.style.left = "-150px";
+                chicken.style.backgroundPosition = "right -260px";
+                break;
+        }
 
 
-const addClass = (el) =>{
-    const id = el.id;
-    console.log(id)
-    switch (id) {
-    case 'up':
-        chicken.classList.toggle("up");
-        break;
-}
-}
 
-    
-    btns.forEach( (btn) => {    
-        btn.addEventListener("click", addClass(btn));          
-    }); 
-        
- console.log(btns);
+    })
+})
     
 
